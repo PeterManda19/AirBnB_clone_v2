a19d489ab868fe28faf2fb2b6f1665dbc55f2543
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-04-23 15:50:36 +0200

    c route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,45 +1,34 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application
-"""
+"""Flask framework"""
 
-from flask import Flask
-
-# Create a new Flask application instance
+from flask import Flask, render_template
 app = Flask(__name__)
 
-
-# Define a route for the root URL path '/'
-# Set strict_slashes=False to allow for URLs with or without a trailing slash
+# define a route for the root URL path '/'
+# and set strict_slashes=False to allow for URLs with or without a trailing slash
 @app.route('/', strict_slashes=False)
 def hello():
-    """
-    Displays 'Hello HBNB!' when the root URL is accessed
-    """
+    """display Hello HBNB!"""
     return 'Hello HBNB!'
 
 
-# Define a route for the '/hbnb' URL path
-# Set strict_slashes=False to allow for URLs with or without a trailing slash
+# define a route for '/hbnb' URL path
+# and set strict_slashes=False to allow for URLs with or without a trailing slash
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    Displays 'HBNB' when the '/hbnb' URL is accessed
-    """
+    """display HBNB"""
     return 'HBNB'
 
 
-# Define a route for the '/c/<text>' URL path
-# Set strict_slashes=False to allow for URLs with or without a trailing slash
+# define a route for '/c/<text>' URL path
+# where <text> is a variable that can be any string
+# and set strict_slashes=False to allow for URLs with or without a trailing slash
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """
-    Displays 'C ' followed by the value of the text variable (replace underscore _ symbols with a space)
-    when the '/c/<text>' URL is accessed
-    """
+def c(text):
+    """display C followed by the text"""
     return 'C {}'.format(text.replace('_', ' '))
 
-
-# Start the Flask application, listening on host '0.0.0.0' and port 5000
+# start the Flask application, listening on host '0.0.0.0' and port 5000
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
