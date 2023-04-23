831a27eee4945a514627141d8f8ae881e43f6ce1
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-04-23 15:34:57 +0200

    hello route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-"""Flask framework """
+"""
+Flask framework
+"""
 
 from flask import Flask
 
 # create a new Flask application instance
 app = Flask(__name__)
 
-# define a route for the root URL path '/' 
-# and set strict_slashes=False to allow for URLs with or without a trailing slash
+
+# define a route for the root URL path '/'
+# set strict_slashes=False to allow for URLs with or without a trailing slash
 @app.route('/', strict_slashes=False)
 def hello():
+    """
+    Displays 'Hello HBNB!' when the root URL is accessed
+    """
     # return a simple string that says "Hello HBNB!"
     return 'Hello HBNB!'
+
 
 # start the Flask application, listening on host '0.0.0.0' and port 5000
 if __name__ == '__main__':
